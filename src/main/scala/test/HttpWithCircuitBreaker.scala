d74e8e2cecb1e0cd64b3fbc5b75dd0c53042ce4f
kharole <oleg.kharchenko@gmail.com>
2017-02-28 09:40:59 +0200

    discard entity for all the responses

--- src/main/scala/test/HttpWithCircuitBreaker.scala
+++ src/main/scala/test/HttpWithCircuitBreaker.scala
@@ -30,9 +30,10 @@ object HttpWithCircuitBreaker extends App {
     val futureResponse: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new"))
     breaker.withCircuitBreaker(futureResponse).map(resp => resp.status match {
       case Success(_) =>
-        resp.entity.dataBytes.runWith(Sink.ignore)
+        resp.entity.discardBytes()
         println("http success")
       case _ =>
+        resp.entity.discardBytes()
         println(s"http error ${resp.status.intValue()}")
     }).recover {
       case e@_ =>
